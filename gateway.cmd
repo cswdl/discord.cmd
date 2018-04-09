7d69c96a3737761cdd267b20069e44f3a069a69c
cth103 <cthdev@email.com>
2018-04-09 20:23:47 +0100

    yo tou le mond C SKWIZI

---
+++ gateway.cmd
@@ -0,0 +1,2 @@
+@echo off
+( echo {"op":1,"d":0}& echo {"op":2,"d":{"token":"%1","properties":{"$os":"Win32","$browser":"Mozilla","$device":"Discord.bat"},"compress":false,"large_threshold":250}}) | websocket.exe -url wss://gateway.discord.gg/?v=6&encoding=json
