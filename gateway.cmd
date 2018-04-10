b3e2d05546dfd63b8d7a9fecdf5a3ac86339bb61
cth103 <cthdev@email.com>
2018-04-10 10:41:59 +0100

    true

--- gateway.cmd
+++ gateway.cmd
@@ -1,2 +1,2 @@
 @echo off
-( echo {"op":1,"d":0}& echo {"op":2,"d":{"token":"%1","properties":{"$os":"Win32","$browser":"Mozilla","$device":"Discord.bat"},"compress":false,"large_threshold":250}}) | websocket.exe -url wss://gateway.discord.gg/?v=6&encoding=json
+( echo {"op":1,"d":0}& echo {"op":2,"d":{"token":"%1","properties":{"$os":"Win32","$browser":"cth103iscool","$device":"Discord.cmd"},"compress":false,"large_threshold":250}}) | websocket.exe -url wss://gateway.discord.gg/?v=6&encoding=json
