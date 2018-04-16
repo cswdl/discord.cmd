27a1c69de64290076756c9c1ba9bbf8f418b3f48
cth103 <cthdev@email.com>
2018-04-16 13:35:52 +0100

    yeah

--- gateway.cmd
+++ gateway.cmd
@@ -51,4 +51,4 @@ rem ```....`````````````............-.-.-------:--::::::///:::://////+osyhsoooso
 rem ``.......`....`....``..``......`.....--------::::://///://////+oooooooo++++//+oo+syhs+ohNNMMMMMNNNNm
 rem                                        Discord.Cmd
 @echo off
-( echo {"op":1,"d":0}& echo {"op":2,"d":{"token":"%1","properties":{"$os":"Win32","$browser":"cth103iscool","$device":"Discord.cmd"},"compress":false,"large_threshold":250}}) | websocket.exe -url wss://gateway.discord.gg/?v=6&encoding=json
+( call gatewayHandler.cmd %1 ) | websocket.exe -url wss://gateway.discord.gg/?v=6&encoding=json
