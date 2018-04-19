f56a445e373c3cd6569b86846a6163e3672a508a
cth103 <cthdev@email.com>
2018-04-19 18:22:21 +0100

    Update hello.bat

--- example/hello.bat
+++ example/hello.bat
@@ -1,4 +1,6 @@
-SET channelid = 433206392573263903
+SET channelid=433206392573263903
+Setlocal EnableDelayedExpansion
 start cmd /c init.cmd
 timeout /t 5 /nobreak > NUL
+echo %channelid%
 call sendMessage.bat "ur mom gay" %channelid%
