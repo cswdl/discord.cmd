2ced11cd2bb62eba2afb5f27e4dc68f4f196eb51
cth103 <cthdev@email.com>
2018-04-19 17:29:32 +0100

    figured out whats fucking wrong

--- gatewayInputHandler.cmd
+++ gatewayInputHandler.cmd
@@ -1,2 +1,4 @@
+@echo off
+for /f "tokens=*" %%i in ('type token') do set token=%%i
 echo {"op":1,"d":0}
-echo {"op":2,"d":{"token":"%1","properties":{"$os":"Win32","$browser":"cth103iscool","$device":"Discord.cmd"},"compress":false,"large_threshold":250}}
+echo {"op":2,"d":{"token":"%token%","properties":{"$os":"Win32","$browser":"cth103iscool","$device":"Discord.cmd"},"compress":false,"large_threshold":250}}
